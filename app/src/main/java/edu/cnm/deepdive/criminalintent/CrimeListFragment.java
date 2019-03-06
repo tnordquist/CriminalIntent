f666277d3a5749732edbbc8e4486630844aa3d2d
Todd A Nordquist <toddnordquist144@gmail.com>
2019-03-06 07:05:12 -0700

    Added ch. 9 lessons: imageview.

--- app/src/main/java/edu/cnm/deepdive/criminalintent/CrimeListFragment.java
+++ app/src/main/java/edu/cnm/deepdive/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
@@ -48,6 +49,7 @@ public class CrimeListFragment extends Fragment {
 
     private TextView mTitleTextView;
     private TextView mDateTextView;
+    private ImageView mSolvedImageView;
     private Crime mCrime;
 
     public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
@@ -56,12 +58,14 @@ public class CrimeListFragment extends Fragment {
 
       mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
       mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+      mSolvedImageView=(ImageView) itemView.findViewById(R.id.crime_solved);
     }
 
     public void bind(Crime crime) {
       mCrime = crime;
       mTitleTextView.setText(mCrime.getTitle());
       mDateTextView.setText(mCrime.getDate().toString());
+      mSolvedImageView.setVisibility(crime.isSolved()?View.VISIBLE : View.GONE);
     }
 
     @Override
